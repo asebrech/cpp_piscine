3c25813d27a98f1e6c142184cbccfe15abc9a023
Aloïs Sebrechts <alois@Aloiss-MacBook-Pro.local>
2022-03-15 15:23:31 +0100

    fix ex01

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 19:16:53 by asebrech          #+#    #+#             */
-/*   Updated: 2022/03/15 10:55:34 by asebrech         ###   ########.fr       */
+/*   Updated: 2022/03/15 15:22:41 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,23 @@
 
 int	main(void)
 {
+	std::string	strs[] = {"finish", " the ", "fight"};
+
+	std::cout << "Testing with std::string [] :" << std::endl;
+	iter(strs, 3, &ft_print);
+	std::cout << std::endl;
+
+	std::cout << std::endl;
+
 	char	str[] = "this is the way";
 
-	std::cout << "Testing with char * :" << std::endl;
+	std::cout << "Testing with char [] :" << std::endl;
 	iter(str, 15, &ft_print);
 	std::cout << std::endl;
 
 	std::cout << std::endl;
 
-	std::cout << "Testing with int * :" << std::endl;
+	std::cout << "Testing with int [] :" << std::endl;
 	int	tab[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 	iter(tab, 10, &ft_print);
