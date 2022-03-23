7cbbe2c909f27ae72bc752c2c73222584209b902
Aloïs Sebrechts <alois@Aloiss-MacBook-Pro.local>
2022-03-23 12:13:44 +0100

    ex02

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 09:59:50 by asebrech          #+#    #+#             */
+/*   Updated: 2022/03/23 11:50:56 by asebrech         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+
+template <typename T>
+class	MutantStack : public std::stack<T>
+{
+	public:
+
+		MutantStack<T>(void) {}
+		MutantStack<T>(MutantStack<T> const &src)
+		{
+			*this = src;
+		}
+		virtual ~MutantStack<T>(void) {}
+
+		MutantStack<T>	&operator=(MutantStack<T> const &rhs)
+		{
+			if (this != &rhs)
+			{
+				std::stack<T>::operator=(rhs);
+			}
+
+			return (*this);
+		}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator	begin() { return std::begin(this->c); }
+		iterator	end() { return std::end(this->c); }
+};
+
+#endif
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 19:16:53 by asebrech          #+#    #+#             */
+/*   Updated: 2022/03/23 12:11:45 by asebrech         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <list>
+
+int	main(void)
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);	
+
+
+	std::cout << std::endl << "Comparing with a list :" << std::endl << std::endl;
+
+	
+	std::list<int> mlist;
+
+	mlist.push_back(5);
+	mlist.push_back(17);
+
+	std::cout << mlist.back() << std::endl;
+
+	mlist.pop_back();
+
+	std::cout << mlist.size() << std::endl;
+
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	//[...]
+	mlist.push_back(0);
+
+	std::list<int>::iterator itl = mlist.begin();
+	std::list<int>::iterator itel = mlist.end();
+
+	++itl;
+	--itl;
+
+	while (itl != itel)
+	{
+		std::cout << *itl << std::endl;
+		++itl;
+	}
+	std::list<int> l(mlist);	
+}
